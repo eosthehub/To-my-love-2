0ccf23717ae0a133b2604852840db4a37bb347d9
eosthehub <machadocpatricia4@gmail.com>
2025-01-11 08:13:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-// Lista de palavras-chave e os sites correspondentes
-const keywords = {
-        "textos": "https://eosthehub.github.io/To-my-love/Central.html",
+// Função para redirecionar os botões
+function redirect(url) {
+    window.location.href = url;
+}
+
+// Área de pesquisa funcional
+document.getElementById("search-button").addEventListener("click", function () {
+    const query = document.getElementById("search-input").value.toLowerCase().trim();
+
+    // Redirecionamento com palavras-chave
+    const keywords = {
+                "textos": "https://eosthehub.github.io/To-my-love/Central.html",
     "jogos": "https://eosthehub.github.io/To-my-love/jogos.html",
     "spotify": "https://open.spotify.com/playlist/1bN2og7ffd4otRABJQakPx?si=E0WXqAb6TECegd8VkS3ZQQ&pi=RohRmneIR9qNa",        
     "videos": "https://eosthehub.github.io/To-my-love/animacao.html",    
@@ -8,17 +17,11 @@ const keywords = {
     "fotos": "https://eosthehub.github.io/To-my-love/password.html",
     "nudes": "https://eosthehub.github.io/To-my-love/password.html",  
     "buceta": "https://eosthehub.github.io/To-my-love/password.html",
-};
+    };
 
-// Função para lidar com a submissão do formulário
-document.getElementById("search-form").addEventListener("submit", function (event) {
-    event.preventDefault(); // Evita o envio padrão do formulário
-    const query = document.getElementById("search-input").value.toLowerCase().trim(); // Obtém a pesquisa
-
-    // Verifica se a palavra-chave existe no objeto
     if (keywords[query]) {
-        window.location.href = keywords[query]; // Redireciona para o site correspondente
+        redirect(keywords[query]);
     } else {
-        alert("Nenhum resultado encontrado para: " + query); // Alerta caso não encontre a palavra-chave
+        alert("Nenhum resultado encontrado para sua pesquisa!");
     }
 });
